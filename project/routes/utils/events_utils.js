056a83a5d188e6bc98d305c7fafca085a98c1548
tomervaron <tomervar@post.bgu.ac.il>
2021-06-06 14:03:46 +0300

    stage_fix

--- project/routes/utils/events_utils.js
+++ project/routes/utils/events_utils.js
@@ -13,5 +13,21 @@ async function addEventToMatch(team_id,event){
         values ('${date}', '${time}','${minute}','${game_event}')`
     );
 }
+async function getAllMatchEvents(match_id){
+    const eventCalender = [];
+    const events = await DButils.execQuery(`SELECT * FROM dbo.Events where MatchId='${match_id}'`);
+    for(const event of events){
+        let event_json ={
+            Event_Id:event.Event_Id,
+            event_date:event.event_date,
+            event_time:event.event_time,
+            minute:event.minute,
+            game_event:event.game_event 
+        };
+        eventCalender.push(event_json);
+    }
+    return eventCalender;
+}
 
+exports.getAllMatchEvents = getAllMatchEvents;
 exports.addEventToMatch = addEventToMatch;
